0022d3af92e82fe1eb69c8080f676047c316bdbc
Allan Farca <allan.farca@gmail.com>
2020-04-08 04:54:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+  
+  def initialize (brand)
+    @brand = brand
+  end 
+  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def cobble
+    
+  end 
+  
+end
